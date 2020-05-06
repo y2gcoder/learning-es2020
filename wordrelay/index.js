c952383487a3e43e1d69c4a14106a5e53ef7b4d6
yang <y2gcoder@gmail.com>
2020-05-07 08:55:52 +0900

    ~ 2-3 String and Index

---
+++ wordrelay/index.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#button");
+
+btn.addEventListener("click", () => {
+  const word = document.querySelector("#word").textContent;
+  const input = document.querySelector("#input").value;
+  const lastIndex = word.length - 1;
+  word[lastIndex];
+  input[0];
+});

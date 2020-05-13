d77b000875b7825df6478bc1242a734484fbd990
yang <y2gcoder@gmail.com>
2020-05-13 09:10:15 +0900

    ~4-3 완료

---
+++ numberbaseball/index.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("#input");
+const check = document.querySelector("#check");
+const logs = document.querySelector("#logs");
+
+let count = 0;
+check.addEventListener("click", () => {
+	const value = input.value; // 문자열 ex) '3046' === truthy value
+	// 0, '', NaN, false, null, undefined === falsy value
+  if (value && value.length === 4) {
+		if(answer === value)
+  }
+});

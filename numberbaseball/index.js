52d40cbfa413056b899b5c35100daaf534c09e17
yang <y2gcoder@gmail.com>
2020-05-14 09:12:29 +0900

    ~4-5 완료

--- numberbaseball/index.js
+++ numberbaseball/index.js
@@ -2,11 +2,20 @@ const input = document.querySelector("#input");
 const check = document.querySelector("#check");
 const logs = document.querySelector("#logs");
 
+let answer = [
+  Math.floor(Math.random() * 10),
+  Math.floor(Math.random() * 10),
+  Math.floor(Math.random() * 10),
+  Math.floor(Math.random() * 10),
+];
+answer.join(""); // 3046
+
 let count = 0;
 check.addEventListener("click", () => {
-	const value = input.value; // 문자열 ex) '3046' === truthy value
-	// 0, '', NaN, false, null, undefined === falsy value
+  const value = input.value; // 문자열 ex) '3046' === truthy value
+  // 0, '', NaN, false, null, undefined === falsy value
   if (value && value.length === 4) {
-		if(answer === value)
+    if (answer === value) {
+    }
   }
 });

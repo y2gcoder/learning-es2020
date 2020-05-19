5bfdf5a92f5275e6a279764c12bbfc03f3903df2
y2gcoder <y2gcoder@gmail.com>
2020-05-20 08:46:59 +0900

    4-12 completed 별찍기 아직

--- numberbaseball/index.js
+++ numberbaseball/index.js
@@ -2,7 +2,14 @@ const input = document.querySelector("#input");
 const check = document.querySelector("#check");
 const logs = document.querySelector("#logs");
 
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let numbers = [];
+// for (let n = 0; n <= 9; n++) {
+//   numbers.push(n);
+// }
+// 함수형 프로그래밍
+let numbers = Array(10)
+  .fill()
+  .map((v, i) => i);
 // 범위에 멋어나면 undefined
 let answer = [];
 // let n = 0;
@@ -13,7 +20,7 @@ let answer = [];
 //   n++;
 // }
 for (let n = 0; n <= 3; n += 1) {
-  const index = Math.floor(Math.random() * (10 - n)); // 0~9 정수, 할 때마다 범위도 줄여주어야 함.
+  const index = Math.floor(Math.random() * numbers.length); // 0~9 정수, 할 때마다 범위도 줄여주어야 함.
   answer.push(numbers[index]);
   numbers.splice(index, 1); // splice(index, 1) 그 인덱스를 하나 지워준다.
 }
@@ -32,11 +39,23 @@ check.addEventListener("click", () => {
       let strike = 0;
       let ball = 0;
       // entries() 배열 뒤에만 가능
-      for (const [aIndex, aNumber] of answer.entries()) {
-        for (const [iIndex, iString] of input.value.split("").entries()) {
-          // 변수명에 속지 않게 문자열이니까 iString으로!
-          // console.log(aIndex, aNumber, iIndex, iString);
-          if (aNumber === Number(iString)) {
+      // for (const [aIndex, aNumber] of answer.entries()) {
+      //   for (const [iIndex, iString] of input.value.split("").entries()) {
+      //     // 변수명에 속지 않게 문자열이니까 iString으로!
+      //     // console.log(aIndex, aNumber, iIndex, iString);
+      //     if (aNumber === Number(iString)) {
+      //       if (aIndex === iIndex) {
+      //         strike += 1;
+      //       } else {
+      //         ball += 1;
+      //       }
+      //     }
+      //   }
+      // }
+      // 숙제
+      for (let aIndex = 0; aIndex < answer.length; aIndex++) {
+        for (let iIndex = 0; iIndex < input.value.length; iIndex++) {
+          if (answer[aIndex] === Number(input.value[iIndex])) {
             if (aIndex === iIndex) {
               strike += 1;
             } else {
@@ -45,8 +64,15 @@ check.addEventListener("click", () => {
           }
         }
       }
-      logs.appendChild(
-        document.createTextNode(`${input.value}: ${strike} strike ${ball} ball`)
+      const message = document.createTextNode(
+        `${input.value}: ${strike} strike ${ball} ball`
+      );
+      // logs.appendChild(message);
+      // logs.appendChild(document.createElement("br"));
+      // 새 기능 append
+      logs.append(
+        `${input.value}: ${strike} strike ${ball} ball`,
+        document.createElement("br")
       );
       if (count > 10) {
         // 문자열에서는 웬만하면 문자열로 만들어서 넣어주자 ex) 배열 -> 문자열
@@ -94,3 +120,15 @@ check.addEventListener("click", () => {
 //   console.log(" ".repeat(Math.abs(num)) + "*".repeat(5 - Math.abs(num)));
 //   num -= 2;
 // }
+
+// 4-12 별찍기 숙제
+//    *
+//  ** **
+//***   ***
+//  ** **
+//    *
+//4   1   4
+//2 2 1 2 2
+//3   3   3
+//2 2 1 2 2
+//4   1   4

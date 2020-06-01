6061cf3eaa241eb3247a81c0602d3287445e0d4c
y2gcoder <y2gcoder@gmail.com>
2020-06-01 17:49:24 +0900

    5-2 완료

--- lotto/index.js
+++ lotto/index.js
@@ -1,3 +1,6 @@
 // fill() 은 undefined로 채움. 괄호 안에 채우면 그 대로 들어감.
 // map() 배열에서 1대 1로 꺼내와서 함.
-const candidate = Array(45).fill().map();
+// map()의 callback 함수(arrow function)에서 매개변수 첫 번째는 값, 두 번째는 index가 됨
+const candidate = Array(45)
+  .fill()
+  .map((v, i) => i + 1);
